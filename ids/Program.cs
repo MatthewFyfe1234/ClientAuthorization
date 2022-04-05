ef1ff4eac3740fff1eba2254bb21d0932615eba7
Dell Precision <mfyfeatlethenty@hotmail.co.uk>
2022-04-05 20:15:25 +0100

    idserver one

---
+++ ids/Program.cs
@@ -0,0 +1,24 @@
+using ids;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+.AddTestUsers(TestUsers.Users)
+.AddInMemoryClients(Config.Clients)
+.AddInMemoryApiResources(Config.ApiResources)
+.AddInMemoryApiScopes(Config.ApiScopes)
+.AddInMemoryIdentityResources(Config.IdentityResources);
+
+var app = builder.Build();
+app.UseIdentityServer();
+
+app.MapGet("/", () => "Identity server");
+
+app.Run();
---
+++ mvcClient/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvcClient.Models;
+using Newtonsoft.Json;
+
+namespace mvcClient.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Weather()
+    {
+        var data = new List<WeatherData>();
+        using (var client = new HttpClient())
+        {
+            var result = client.GetAsync("https://localhost:5445/weatherforecast").Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var model = result.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject<List<WeatherData>>(model);
+                return View(data);
+            }
+            else
+            {
+                throw new Exception("Unauthorised");
+            }
+        };
+    }
+}
